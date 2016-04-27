9066f96b9582fb5df6e785072bcf92f527204e26
Koen <koen@koenvh.nl>
2016-04-27 16:17:50 +0200

    Added caching

--- get_file.php
+++ get_file.php
@@ -8,32 +8,32 @@
 
 require_once ("includes.php");
 
-$requestURL = (($_GET["file"]));
-//echo $requestURL;
+$requestURL = $_GET["file"];
+$md5name = md5($requestURL);
+if(file_exists(CONVERT_FOLDER . "/" . $md5name . ".mp3") == false) {
+    $response = $client->request('GET', $requestURL);
 
-//$requestURL = str_replace(' ', '%20', $requestURL);
-$response = $client->request('GET', $requestURL);
-
-header("HTTP/1.0 " . $response["statusCode"]);
-//header('Content-Type: audio/mpeg');
-//header('Content-Disposition: filename="'. end(explode('/', $requestURL)) . '"');
-header('Content-length: ' . $response["headers"]["content-length"][0]);
-header('Cache-Control: no-cache');
-header("Content-Transfer-Encoding: binary");
-
-if($response["headers"]["content-type"][0] == "audio/mpeg"){
-    header('Content-Type: audio/mpeg');
-    echo $response["body"];
-} else {
+    header("HTTP/1.0 " . $response["statusCode"]);
     //header('Content-Type: audio/mpeg');
-    //echo $response["body"];
-    //die();
+    //header('Content-Disposition: filename="'. end(explode('/', $requestURL)) . '"');
+    header('Content-length: ' . $response["headers"]["content-length"][0]);
+    //header('Cache-Control: no-cache');
+    header("Content-Transfer-Encoding: binary");
 
-    //Generate a random name:
-    $randomString = substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 1) . substr(md5(time()), 1);
-    file_put_contents(CONVERT_FOLDER . "/" . $randomString . "", $response["body"]);
-    shell_exec(FFMPEG . " -i " . CONVERT_FOLDER . "/" . $randomString . " -threads auto -aq 3 -vn " . CONVERT_FOLDER . "/" . $randomString . ".mp3");
-    header('Location: ' . CONVERT_FOLDER_RELATIVE . "/" . $randomString . ".mp3");
-    die();
+    if ($response["headers"]["content-type"][0] == "audio/mpeg") {
+        header('Content-Type: audio/mpeg');
+        echo $response["body"];
+        die();
+    } else {
+        //header('Content-Type: audio/mpeg');
+        //echo $response["body"];
+        //die();
+        //Generate a random name:
+        file_put_contents(CONVERT_FOLDER . "/" . $md5name . "", $response["body"]);
+        shell_exec(FFMPEG . " -i " . CONVERT_FOLDER . "/" . $md5name . " -threads auto -aq 3 -vn " . CONVERT_FOLDER . "/" . $md5name . ".mp3");
+
+    }
 }
+header('Location: ' . CONVERT_FOLDER_RELATIVE . "/" . $md5name . ".mp3");
+die();
 //echo var_dump($response);
