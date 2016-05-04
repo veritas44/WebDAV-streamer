3f0e357be4088e7913e9a33a5c14a04c30619688
Koen <koen@koenvh.nl>
2016-05-04 03:46:39 +0200

    Made username case-insensitive

--- class/auth.php
+++ class/auth.php
@@ -6,12 +6,13 @@ class Auth {
 
     function __construct($users)
     {
-        $this->users = $users;
+        $this->users = array_change_key_case($users);
     }
+
     function login($username, $password){
-        if(key_exists($username, $this->users)){
-            if($this->users[$username]["password_streamer"] == $password){
-                $this->username = $username;
+        if(key_exists(strtolower($username), $this->users)){
+            if($this->users[strtolower($username)]["password_streamer"] == $password){
+                $this->username = strtolower($username);
                 return "success";
             }
         }
