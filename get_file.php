ded1fc845c94f99dc4341a05051a33d736e6891c
Koen <koen@koenvh.nl>
2016-06-27 00:26:39 +0200

    Fixed interface locking up while converting media

--- get_file.php
+++ get_file.php
@@ -8,6 +8,7 @@
 
 require_once ("includes.php");
 
+session_write_close();
 $requestURL = ($_GET["file"]);
 
 //die($requestURL);
@@ -21,10 +22,16 @@ $md5name = md5($auth->username . $requestURL);
 $extension = "." . pathinfo(urldecode($requestURL), PATHINFO_EXTENSION);
 //print_r($_GET);
 $supportedMimeTypes = json_decode($_GET["support"], true);
+$lockedFile = CONVERT_FOLDER . "/" . $md5name . ".lock";
+
+//Sleep while the lock file is still present:
+while (file_exists($lockedFile)){
+    sleep(1);
+}
 
 if(file_exists(CONVERT_FOLDER . "/" . $md5name . $extension) == false) {
     if(file_exists(CONVERT_FOLDER . "/" . $md5name . ".mp3") == false) {
-
+        file_put_contents($lockedFile, "locked");
         $response = $client->request('GET', $requestURL);
         if ($response["statusCode"] >= 400) {
             $response = $client->request('GET', Sabre\HTTP\decodePath($requestURL));
@@ -49,6 +56,7 @@ if(file_exists(CONVERT_FOLDER . "/" . $md5name . $extension) == false) {
             file_put_contents(CONVERT_FOLDER . "/" . $md5name, $response["body"]);
             shell_exec(FFMPEG . " -i " . CONVERT_FOLDER . "/" . $md5name . " -threads 0 -map_metadata 0 -id3v2_version 3 -vn " . CONVERT_FOLDER . "/" . $md5name . ".mp3");
         }
+        unlink($lockedFile);
     } else {
         $extension = ".mp3";
     }
--- get_video.php
+++ get_video.php
@@ -8,12 +8,19 @@
 
 require_once ("includes.php");
 
+session_write_close();
 $requestURL = ($_GET["file"]);
 $response = $client->request('HEAD', $requestURL);
 
 $extension = "." . pathinfo(urldecode($requestURL), PATHINFO_EXTENSION);
 $md5name = md5($auth->username . $requestURL);
 $supportedMimeTypes = json_decode($_GET["support"], true);
+$lockedFile = CONVERT_FOLDER . "/" . $md5name . ".lock";
+
+//Sleep while the lock file is still present:
+while (file_exists($lockedFile)){
+    sleep(1);
+}
 
 file_put_contents(CONVERT_FOLDER . "/" . "progress.txt", "Loading " . urldecode($_GET["file"]) . "\r\n");
 
@@ -29,6 +36,7 @@ if (array_key_exists($response["headers"]["content-type"][0], $supportedMimeType
     die();
 } else {
     if(file_exists(CONVERT_FOLDER . "/" . $md5name . ".mp4") == false) {
+        file_put_contents($lockedFile, "locked");
         $response = $client->request('GET', $requestURL);
 
         header("HTTP/1.0 " . $response["statusCode"]);
@@ -54,6 +62,7 @@ if (array_key_exists($response["headers"]["content-type"][0], $supportedMimeType
             file_put_contents(CONVERT_FOLDER . "/" . $md5name . "", $response["body"]);
             shell_exec(FFMPEG . " -i " . CONVERT_FOLDER . "/" . $md5name . " -threads 0 " . CONVERT_FOLDER . "/" . $md5name . ".mp4 1>" . CONVERT_FOLDER . "/progress.txt 2>&1"); // 1>" . CONVERT_FOLDER . "/" . $md5name . ".progress 2>&1
         }
+        unlink($lockedFile);
     }
     header('Location: ' . CONVERT_FOLDER_RELATIVE . "/" . $md5name . ".mp4");
     die();
